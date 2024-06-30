a9f0fada632187f25bf27bbe1c0c2e3b68ca5331
Mouhssine Saidi <s3idi.mouhssine@gmail.com>
2024-06-30 06:59:09 +0100

    modify the solving

--- longest_word_in_string.py
+++ longest_word_in_string.py
@@ -1,16 +1,13 @@
 def longest_word(words):
   
-  longestLen = 0
   longestWord = ""
-  splArray = words.split(" ")
+  word_list = words.split(" ")
   
-  for i in range(len(splArray)):
-    if len(splArray[i]) > longestLen:
-      longestWord = splArray[i]
-      longestLen = len(longestWord)
+  for word in word_list:
+    if len(word) > len(longestWord):
+      longestWord = word
   return longestWord
 
 
 sentence = "This is a common interview question"
-#longest_word(sentence)
 print(longest_word(sentence))
